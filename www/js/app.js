156e03d23243f556a759bb5163754f8dd81e1ea8
plopushin <lopushin@ualberta.ca>
2017-02-08 14:13:55 -0700

    Show link and source on view, add reddit json data in app.js

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,20 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+
+angular.module('myreddit', ['ionic'])
+
+.controller('RedditCtrl', function($http, $scope) {
+
+  $scope.stories = [];
+
+  $http.get('https://www.reddit.com/r/wholesomememes/new.json')
+    .success(function(response) {
+      angular.forEach(response.data.children, function(child) {
+        $scope.stories.push(child.data);
+      });
+    });
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
