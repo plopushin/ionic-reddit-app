eae004a478964551b5c20b976c088368e20535c5
plopushin <lopushin@ualberta.ca>
2017-02-09 12:44:48 -0700

    Add ininite scroll and refresh function

--- www/js/app.js
+++ www/js/app.js
@@ -10,13 +10,37 @@ angular.module('myreddit', ['ionic', 'angularMoment'])
 
   $scope.stories = [];
 
-  $http.get('https://www.reddit.com/r/wholesomememes/new.json')
-    .success(function(response) {
-      angular.forEach(response.data.children, function(child) {
-        $scope.stories.push(child.data);
+  function loadStories(params, callback) {
+    $http.get('https://www.reddit.com/r/wholesomememes/new.json', {params:params})
+      .success(function(response) {
+        var stories = [];
+        angular.forEach(response.data.children, function(child) {
+          $scope.stories.push(child.data);
+        });
+        callback(stories);
       });
+  }
+  $scope.loadOlderStories = function () {
+    var params = {};
+    if ($scope.stories.length > 0) {
+      params['after'] = $scope.stories[$scope.stories.length - 1].name;
+    }
+    loadStories(params, function(olderStories) {
+      $scope.stories = $scope.stories.concat(olderStories);
+      $scope.$broadcast('scroll.infiniteScrollComplete');
     });
-})
+
+    }
+
+  $scope.loadNewerStories = function () {
+    var parms = {'before': $scope.stories[0].name};
+    loadStories(params, function (newerStories) {
+      $scope.stories = newerStories.concat($scope.stories);
+      $scope.$broadcast('scroll.refreshComplete');
+    })
+  };
+
+  })
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
